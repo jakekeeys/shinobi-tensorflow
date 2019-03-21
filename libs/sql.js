8735d9b27ba3d615b655d4a1c1bde24225a682af
Moe <github@m03.ca>
2019-03-21 16:50:46 -0700

    fix Timelapse table creation

--- libs/sql.js
+++ libs/sql.js
@@ -119,7 +119,7 @@ module.exports = function(s,config){
             if(err)console.error(err)
         },true)
         //add Schedules table, will remove in future
-        s.sqlQuery("CREATE TABLE IF NOT EXISTS `Timelapses` (`ke`varchar(50)NOT NULL,`mid`varchar(50)NOT NULL,`details`longtext,`date`date NOT NULL,`time`timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,`end`timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,`size`int(11)NOT NULL)" + mySQLtail + ';',[],function(err){
+        s.sqlQuery("CREATE TABLE IF NOT EXISTS `Timelapses` (`ke`varchar(50)NOT NULL,`mid`varchar(50)NOT NULL,`details`longtext,`date`date NOT NULL,`time`timestamp NOT NULL,`end`timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,`size`int(11)NOT NULL)" + mySQLtail + ';',[],function(err){
             if(err)console.error(err)
         },true)
         //add Cloud Videos table, will remove in future
