9ff69b28b3cdfceefa0c0a65dbab5ac092bc7e96
Moe <github@m03.ca>
2019-03-21 21:22:27 -0700

    add more config parameters for branding on login

--- libs/config.js
+++ libs/config.js
@@ -10,11 +10,12 @@ module.exports = function(s){
     }
     //config defaults
     if(config.showPoweredBy === undefined){config.showPoweredBy=true}
-    if(config.poweredBy === undefined){config.poweredBy='Powered by Shinobi'}
+    if(config.poweredBy === undefined){config.poweredBy='Powered by Shinobi.Systems'}
     if(config.poweredByClass === undefined){config.poweredByClass='margin:15px 0 0 0;text-align:center;color:#777;font-family: sans-serif;text-transform: uppercase;letter-spacing: 3;font-size: 8pt;'}
     if(config.webPageTitle === undefined){config.webPageTitle='Shinobi'}
     if(config.showLoginCardHeader === undefined){config.showLoginCardHeader=true}
     if(config.logoLocation76x76 === undefined){config.logoLocation76x76='libs/img/icon/apple-touch-icon-76x76.png'}
+    if(config.logoLocation76x76Link === undefined){config.logoLocation76x76Link='https://shinobi.video'}
     if(config.logoLocation76x76Style === undefined){config.logoLocation76x76Style='border-radius:50%'}
     if(config.cpuUsageMarker === undefined){config.cpuUsageMarker='%Cpu'}
     if(config.customCpuCommand === undefined){config.customCpuCommand=null}
