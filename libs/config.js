e01acf93812d3d83e382177b7875e8e983b594c3
Moe <github@m03.ca>
2019-03-24 23:46:31 -0700

    modify poweredBy variable name

--- libs/config.js
+++ libs/config.js
@@ -9,9 +9,9 @@ module.exports = function(s){
         config.productType = 'CE'
     }
     //config defaults
-    if(config.showPoweredBy === undefined){config.showPoweredBy=true}
-    if(config.poweredBy === undefined){config.poweredBy='Powered by Shinobi.Systems'}
-    if(config.poweredByClass === undefined){config.poweredByClass='margin:15px 0 0 0;text-align:center;color:#777;font-family: sans-serif;text-transform: uppercase;letter-spacing: 3;font-size: 8pt;'}
+    if(config.showPoweredByShinobi === undefined){config.showPoweredByShinobi=true}
+    if(config.poweredByShinobi === undefined){config.poweredByShinobi='Powered by Shinobi.Systems'}
+    if(config.poweredByShinobiClass === undefined){config.poweredByShinobiClass='margin:15px 0 0 0;text-align:center;color:#777;font-family: sans-serif;text-transform: uppercase;letter-spacing: 3;font-size: 8pt;'}
     if(config.webPageTitle === undefined){config.webPageTitle='Shinobi'}
     if(config.showLoginCardHeader === undefined){config.showLoginCardHeader=true}
     if(config.logoLocation76x76 === undefined){config.logoLocation76x76='libs/img/icon/apple-touch-icon-76x76.png'}
