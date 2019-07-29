934159786a1472593d92b340fa590572212c18ce
Moe <github@m03.ca>
2019-07-29 13:10:08 -0700

    log Shinobi version on start and show in Superuser panel+

--- libs/version.js
+++ libs/version.js
@@ -2,12 +2,16 @@ var exec = require('child_process').exec
 module.exports = function(s,config,lang,app,io){
     var getRepositoryCommitId = function(callback){
         exec(`git rev-parse HEAD`,function(err,response){
-            var data = response.toString()
-            var isGitRespository = false
-            if(data.indexOf('not a git repository') === -1){
-                s.currentVersion = data
-                isGitRespository = true
-                s.systemLog(`Current Version : ${s.currentVersion}`)
+            if(response){
+                var data = response.toString()
+                var isGitRespository = false
+                if(data.indexOf('not a git repository') === -1){
+                    s.currentVersion = data
+                    isGitRespository = true
+                    s.systemLog(`Current Version : ${s.currentVersion}`)
+                }
+            }else if(err){
+                s.debugLog('Git is not installed.')
             }
             if(callback)callback(!isGitRespository,data)
         })
