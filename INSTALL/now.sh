b18b81c4f555e8419b2b40f30b0028f588f94f53
Moe <github@m03.ca>
2019-08-17 10:07:44 -0700

    add CentOS quick install to launcher

--- INSTALL/now.sh
+++ INSTALL/now.sh
@@ -9,6 +9,7 @@ echo "2. CentOS"
 echo "3. MacOS"
 echo "4. FreeBSD"
 echo "5. OpenSUSE"
+echo "6. CentOS - Quick Install"
 echo "========"
 read oschoicee
 case $oschoicee in
@@ -18,19 +19,23 @@ sh INSTALL/ubuntu.sh
   ;;
 "2")
 chmod +x INSTALL/centos.sh
-INSTALL/centos.sh
+sh INSTALL/centos.sh
   ;;
 "3")
 chmod +x INSTALL/macos.sh
-INSTALL/macos.sh
+sh INSTALL/macos.sh
   ;;
 "4")
 chmod +x INSTALL/freebsd.sh
-INSTALL/freebsd.sh
+sh INSTALL/freebsd.sh
   ;;
 "5")
 chmod +x INSTALL/opensuse.sh
-INSTALL/opensuse.sh
+sh INSTALL/opensuse.sh
+  ;;
+"6")
+chmod +x "INSTALL/CentOS - Quick Install.sh"
+sh "INSTALL/CentOS - Quick Install.sh"
   ;;
 
 *)
