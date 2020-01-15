4a69a1aa8e709e1d1e3167f842a1d11f24133572
Moe <github@m03.ca>
2020-01-15 13:47:59 -0800

    adjust response headers
    - make Access-Control-Allow-Origin : *
    - add no-caching headers to streams over http

--- libs/webServerPaths.js
+++ libs/webServerPaths.js
@@ -71,7 +71,7 @@ module.exports = function(s,config,lang,app,io){
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: true}));
     app.use(function (req,res,next){
-        res.header("Access-Control-Allow-Origin",req.headers.origin);
+        res.header("Access-Control-Allow-Origin",'*');
         next()
     })
     app.set('views', s.mainDirectory + '/web');
