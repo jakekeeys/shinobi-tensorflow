a09a47e762bd6c590700672f2a76fd802b6e623f
Moe <github@m03.ca>
2020-01-17 19:44:10 -0800

    Update INSTALL.sh for Tensorflow plugin

--- plugins/tensorflow/INSTALL.sh
+++ plugins/tensorflow/INSTALL.sh
@@ -1,20 +1,11 @@
 #!/bin/bash
-mkdir data
-mkdir data/inception
-chmod -R 777 data
-wget https://cdn.shinobi.video/weights/inception5h.zip -O inception5h.zip
-unzip inception5h.zip -d data/inception
-if [ $(dpkg-query -W -f='${Status}' opencv_version 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
-    echo "Shinobi - Do ypu want to let the `opencv4nodejs` npm package install OpenCV? "
-    echo "Only do this if you do not have OpenCV already or will not use a GPU (Hardware Acceleration)."
-    echo "(y)es or (N)o"
-    read nodejsinstall
-    if [ "$nodejsinstall" = "y" ] || [ "$nodejsinstall" = "Y" ]; then
-        export OPENCV4NODEJS_DISABLE_AUTOBUILD=0
-    else
-        export OPENCV4NODEJS_DISABLE_AUTOBUILD=1
-    fi
+echo "Shinobi - Do you want to install tensorflowjs with GPU support? "
+echo "(y)es or (N)o"
+read nodejsinstall
+if [ "$nodejsinstall" = "y" ] || [ "$nodejsinstall" = "Y" ]; then
+    npm install @tensorflow/tfjs-node-gpu --unsafe-perm
 else
-    export OPENCV4NODEJS_DISABLE_AUTOBUILD=1
+    npm install @tensorflow/tfjs-node --unsafe-perm
 fi
-npm install opencv4nodejs moment express canvas@1.6 --unsafe-perm
+npm install @tensorflow-models/coco-ssd --unsafe-perm
+npm install buffer-to-uint8array
