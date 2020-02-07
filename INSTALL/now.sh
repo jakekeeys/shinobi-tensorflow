e8e122c6dec0542a5b2c1eafffd4555b2fe2856a
Moe <github@m03.ca>
2020-02-07 11:53:13 -0800

    Update Installer chooser

--- INSTALL/now.sh
+++ INSTALL/now.sh
@@ -4,38 +4,43 @@ echo "========"
 echo "Select your OS"
 echo "If your OS is not on the list please refer to the docs."
 echo "========"
-echo "1. Ubuntu"
-echo "2. CentOS"
-echo "3. MacOS"
-echo "4. FreeBSD"
-echo "5. OpenSUSE"
-echo "6. CentOS - Quick Install"
+echo "1. Ubuntu - Fast and Touchless"
+echo "2. Ubuntu - Advanced"
+echo "3. CentOS"
+echo "4. CentOS - Quick Install"
+echo "5. MacOS"
+echo "6. FreeBSD"
+echo "7. OpenSUSE"
 echo "========"
 read oschoicee
 case $oschoicee in
 "1")
+chmod +x INSTALL/ubuntu-touchless.sh
+sh INSTALL/ubuntu-touchless.sh
+  ;;
+"2")
 chmod +x INSTALL/ubuntu.sh
 sh INSTALL/ubuntu.sh
   ;;
-"2")
+"3")
 chmod +x INSTALL/centos.sh
 sh INSTALL/centos.sh
   ;;
-"3")
+"4")
+chmod +x "INSTALL/CentOS - Quick Install.sh"
+sh "INSTALL/CentOS - Quick Install.sh" 1
+  ;;
+"5")
 chmod +x INSTALL/macos.sh
 sh INSTALL/macos.sh
   ;;
-"4")
+"6")
 chmod +x INSTALL/freebsd.sh
 sh INSTALL/freebsd.sh
   ;;
-"5")
+"7")
 chmod +x INSTALL/opensuse.sh
 sh INSTALL/opensuse.sh
-  ;;
-"6")
-chmod +x "INSTALL/CentOS - Quick Install.sh"
-sh "INSTALL/CentOS - Quick Install.sh" 1
   ;;
 
 *)
