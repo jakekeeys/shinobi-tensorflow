9f3a0e814fb04ad66b44a9bd0a2ff80d42e24003
Moe <github@m03.ca>
2020-02-07 16:19:00 -0800

    Add CUDA 10 Installer for TensorFlow plugin

---
+++ INSTALL/cuda-10.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+echo "------------------------------------------"
+echo "-- Installing CUDA Toolkit and CUDA DNN --"
+echo "------------------------------------------"
+# Install CUDA Drivers and Toolkit
+if [ -x "$(command -v apt)" ]; then
+    wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-repo-ubuntu1804_10.0.130-1_amd64.deb
+    sudo dpkg -i cuda-repo-ubuntu1804_10.0.130-1_amd64.deb
+    sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
+
+    sudo apt-get update -y
+
+    sudo apt-get -o Dpkg::Options::="--force-overwrite" install cuda-toolkit-10 -y --no-install-recommends
+    sudo apt-get -o Dpkg::Options::="--force-overwrite" install --fix-broken -y
+
+    # Install CUDA DNN
+    wget https://cdn.shinobi.video/installers/libcudnn7_7.6.5.32-1+cuda10.0_amd64.deb -O cuda-dnn.deb
+    sudo dpkg -i cuda-dnn.deb
+    wget https://cdn.shinobi.video/installers/libcudnn7-dev_7.6.5.32-1+cuda10.0_amd64.deb -O cuda-dnn-dev.deb
+    sudo dpkg -i cuda-dnn-dev.deb
+    echo "-- Cleaning Up --"
+    # Cleanup
+    sudo rm cuda-dnn.deb
+    sudo rm cuda-dnn-dev.deb
+fi
+if [ -x "$(command -v yum)" ]; then
+    wget https://developer.download.nvidia.com/compute/cuda/repos/rhel7/x86_64/cuda-repo-rhel7-10.0.130-1.x86_64.rpm
+    sudo rpm -i cuda-repo-rhel7-10.0.130-1.x86_64.rpm
+    sudo yum clean all
+    sudo yum install cuda
+    wget https://cdn.shinobi.video/installers/libcudnn7-7.6.5.32-1.cuda10.0.x86_64.rpm -O cuda-dnn.rpm
+    sudo yum -y localinstall cuda-dnn.rpm
+    wget https://cdn.shinobi.video/installers/libcudnn7-devel-7.6.5.32-1.cuda10.0.x86_64.rpm -O cuda-dnn-dev.rpm
+    sudo yum -y localinstall cuda-dnn-dev.rpm
+    echo "-- Cleaning Up --"
+    sudo rm cuda-dnn.rpm
+    sudo rm cuda-dnn-dev.rpm
+fi
+
+echo "------------------------------"
+echo "Reboot is required. Do it now?"
+echo "------------------------------"
+echo "(y)es or (N)o. Default is No."
+read rebootTheMachineHomie
+if [ "$rebootTheMachineHomie" = "y" ] || [ "$rebootTheMachineHomie" = "Y" ]; then
+    sudo reboot
+fi
