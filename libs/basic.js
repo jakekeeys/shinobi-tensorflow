45d256cbae5e208b4ce359bad952c90f279c5107
Moe <github@m03.ca>
2020-04-04 20:41:39 -0700

    fs.unlink as preliminary deletion function

--- libs/basic.js
+++ libs/basic.js
@@ -221,15 +221,20 @@ module.exports = function(s,config){
             break;
             case'delete':
                 if(!e){return false;}
-                if(s.isWin){
-                    exec('rd /s /q "' + e + '"',{detached: true},function(err){
-                        if(callback)callback(err)
-                    })
-                }else{
-                    exec('rm -rf '+e,{detached: true},function(err){
-                        if(callback)callback(err)
-                    })
-                }
+                fs.unlink(e,(err)=>{
+                    if(err){
+                        s.debugLog(err)
+                    }
+                    if(s.isWin){
+                        exec('rd /s /q "' + e + '"',{detached: true},function(err){
+                            if(callback)callback(err)
+                        })
+                    }else{
+                        exec('rm -rf '+e,{detached: true},function(err){
+                            if(callback)callback(err)
+                        })
+                    }
+                })
             break;
             case'deleteFolder':
                 if(!e){return false;}
