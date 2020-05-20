58df790e97694cdc17e74abd5f38c148098a2555
Moe <github@m03.ca>
2020-05-20 15:06:39 -0700

    mergeDeep added
    mergeDeep is the same as Object.assign except it works upon multiple layers of an object

--- web/libs/js/dash2.config.js
+++ web/libs/js/dash2.config.js
@@ -48,6 +48,26 @@ switch($user.details.lang){
         })
     break;
 }
+function isObject(item) {
+  return (item && typeof item === 'object' && !Array.isArray(item));
+}
+window.mergeDeep = function(target, ...sources){
+  if (!sources.length) return target;
+  const source = sources.shift();
+
+  if (isObject(target) && isObject(source)) {
+    for (const key in source) {
+      if (isObject(source[key])) {
+        if (!target[key]) Object.assign(target, { [key]: {} });
+        mergeDeep(target[key], source[key]);
+      } else {
+        Object.assign(target, { [key]: source[key] });
+      }
+    }
+  }
+
+  return mergeDeep(target, ...sources);
+}
 window.getApiPrefix = function(){
     return $.ccio.init('location',$user) + $user.auth_token
 }
