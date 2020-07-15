155b9452857d7b0a9e1ba2700c41070c1da502ef
Moe <github@m03.ca>
2020-07-14 23:49:31 -0700

    fix reoccurrring const groupKey+

--- libs/webServerPaths.js
+++ libs/webServerPaths.js
@@ -1521,8 +1521,6 @@ module.exports = function(s,config,lang,app,io){
                 time = s.utcToLocal(time)
             }
             time = new Date(time)
-            const groupKey = req.params.ke
-            const monitorId = req.params.id
             s.knexQuery({
                 action: "select",
                 columns: "*",
