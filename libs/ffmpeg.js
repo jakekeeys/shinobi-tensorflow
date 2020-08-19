8a6bfecf0531fd2cab41dcb2c6a33087d0502936
Moe <github@m03.ca>
2020-08-19 11:46:29 -0700

    add cuda and opencl hwaccel options

--- libs/ffmpeg.js
+++ libs/ffmpeg.js
@@ -103,6 +103,8 @@ module.exports = function(s,config,lang,onFinish){
                 auto: {label:lang['Auto'],value:'auto'},
                 drm: {label:lang['drm'],value:'drm'},
                 cuvid: {label:lang['cuvid'],value:'cuvid'},
+                cuda: {label:lang['cuda'],value:'cuda'},
+                opencl: {label:lang['opencl'],value:'opencl'},
                 vaapi: {label:lang['vaapi'],value:'vaapi'},
                 qsv: {label:lang['qsv'],value:'qsv'},
                 vdpau: {label:lang['vdpau'],value:'vdpau'},
