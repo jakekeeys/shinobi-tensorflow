d916b8d5cd9c45e7a6dd56233b7117eca12a627b
Moe <github@m03.ca>
2020-08-26 22:40:57 -0700

    Minor cleanup++

--- libs/common.js
+++ libs/common.js
@@ -6,6 +6,6 @@ exports.createQueue = (timeoutInSeconds, queueItemsRunningInParallel) => {
     return async.queue(function(action, callback) {
         setTimeout(function(){
             action(callback)
-        },timeoutInSeconds || 1000)
+        },timeoutInSeconds * 1000 || 1000)
     },queueItemsRunningInParallel || 3)
 }
