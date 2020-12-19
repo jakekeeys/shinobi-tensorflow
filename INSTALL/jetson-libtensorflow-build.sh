1f22f266be87ef068ebf0816249ab05cdf511262
Moe Alam <github@m03.ca>
2020-12-19 09:05:28 -0800

    Add script to build libtensorflow on Jetson Nano

---
+++ INSTALL/jetson-libtensorflow-build.sh
@@ -0,0 +1,24 @@
+## uncommet lines 2 and 3 if you have a 10W power supply on your Jetson Nano.
+# sudo nvpmodel -m 0
+# sudo jetson_clocks
+
+############################
+apt install python3-pip -y
+cd ${HOME}
+mkdir bazel
+cd bazel
+sudo pip3 uninstall -y tensorboard tensorflow
+sudo rm /usr/local/lib/libproto*
+sudo rm /usr/local/bin/protoc
+sudo pip3 uninstall -y protobuf
+sudo rm /usr/local/bin/bazel
+git clone https://github.com/jkjung-avt/jetson_nano.git
+cd jetson_nano
+sh ./install_protobuf-3.8.0.sh
+cd ${HOME}/bazel/jetson_nano
+sh ./install_bazel-3.1.0.sh
+cp ${HOME}/src/bazel-3.1.0-dist/output/bazel /usr/local/bin/bazel
+cd ${HOME}/bazel/jetson_nano
+export PIP_FORMAT=legacy
+apt install python3-h5py-dbg -y
+sh ./install_tensorflow-2.3.0.sh
