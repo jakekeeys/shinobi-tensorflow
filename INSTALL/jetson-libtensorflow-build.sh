1656192440a1880e6745c8a3b8aef68d3751306d
Moe Alam <github@m03.ca>
2020-12-21 08:29:05 -0800

    Update jetson-libtensorflow-build.sh

--- INSTALL/jetson-libtensorflow-build.sh
+++ INSTALL/jetson-libtensorflow-build.sh
@@ -21,5 +21,8 @@ cp ${HOME}/src/bazel-3.1.0-dist/output/bazel /usr/local/bin/bazel
 cd ${HOME}/bazel/jetson_nano
 export PIP_FORMAT=legacy
 apt install python3-h5py-dbg -y
+apt install libblas3 liblapack3 liblapack-dev libblas-dev -y
+apt install libgfortran-8-dev-arm64-cross -y
+sudo apt-get install gfortran -y
 sed -i 's/h5py//g' ./install_tensorflow-2.3.0.sh
 sh ./install_tensorflow-2.3.0.sh
