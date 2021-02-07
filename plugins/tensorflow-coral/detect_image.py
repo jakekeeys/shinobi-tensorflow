fa5245156cde48e4c8dbb31ce01016d35221c051
Mitch Ross <mitchross09@gmail.com>
2021-02-07 19:57:42 +0000

    Clean up detect_image, add comments. Remove unnecessary code.

--- plugins/tensorflow-coral/detect_image.py
+++ plugins/tensorflow-coral/detect_image.py
@@ -1,3 +1,4 @@
+
 # Lint as: python3
 # Copyright 2019 Google LLC
 #
@@ -23,92 +24,59 @@ from io import BytesIO, StringIO
 import time
 import base64
 import json
-import detect
-import tflite_runtime.interpreter as tflite
+from pycoral.adapters import common
+from pycoral.adapters import detect
+from pycoral.utils.dataset import read_label_file
+from pycoral.utils.edgetpu import make_interpreter
 import platform
-
-EDGETPU_SHARED_LIB = {
-    'Linux': 'libedgetpu.so.1',
-    'Darwin': 'libedgetpu.1.dylib',
-    'Windows': 'edgetpu.dll'
-}[platform.system()]
-
-def load_labels(path, encoding='utf-8'):
-    """Loads labels from file (with or without index numbers).
-
-    Args:
-      path: path to label file.
-      encoding: label file encoding.
-    Returns:
-      Dictionary mapping indices to labels.
-    """
-    with open(path, 'r', encoding=encoding) as f:
-        lines = f.readlines()
-        if not lines:
-            return {}
-
-        if lines[0].split(' ', maxsplit=1)[0].isdigit():
-            pairs = [line.split(' ', maxsplit=1) for line in lines]
-            return {int(index): label.strip() for index, label in pairs}
-        else:
-            return {index: line.strip() for index, line in enumerate(lines)}
-
-
-def make_interpreter(model_file):
-    model_file, *device = model_file.split('@')
-    return tflite.Interpreter(
-        model_path=model_file,
-        experimental_delegates=[
-            tflite.load_delegate(EDGETPU_SHARED_LIB,
-                                 {'device': device[0]} if device else {})
-        ])
-
-
-def draw_objects(draw, objs, labels):
-    """Draws the bounding box and label for each object."""
-    for obj in objs:
-        bbox = obj.bbox
-        draw.rectangle([(bbox.xmin, bbox.ymin), (bbox.xmax, bbox.ymax)],
-                       outline='red')
-        draw.text((bbox.xmin + 10, bbox.ymin + 10),
-                  '%s\n%.2f' % (labels.get(obj.id, obj.id), obj.score),
-                  fill='red')
 
 
 def printInfo(text):
     print(json.dumps({"type": "info", "data": text}))
 
+
 def printError(text):
     print(json.dumps({"type": "error", "data": text}))
+
 
 def printData(array, time):
     print(json.dumps({"type": "data", "data": array, "time": time}))
 
+
 def main():
-    labels = load_labels("models/coco_labels.txt")
-    interpreter = make_interpreter("models/ssd_mobilenet_v2_coco_quant_postprocess_edgetpu.tflite")
+    labels = read_label_file("models/coco_labels.txt")
+
+    interpreter = make_interpreter(
+        "models/ssd_mobilenet_v2_coco_quant_postprocess_edgetpu.tflite")
     interpreter.allocate_tensors()
     threshold = 0.4
     printInfo("ready")
     while True:
         line = sys.stdin.readline().rstrip("\n")
         try:
+            #load image from shinobi stream
             rawImage = BytesIO(base64.b64decode(line))
             image = Image.open(rawImage)
-            scale = detect.set_input(interpreter, image.size,
-                                     lambda size: image.resize(size, Image.ANTIALIAS))
-
+            #resize the image for object detection using built in coral code
+            #it will set it to 300x300 and provide a scale for object detection later
+            _, scale = common.set_resized_input(
+                interpreter, image.size, lambda size: image.resize(size, Image.ANTIALIAS))
+        
             start = time.perf_counter()
             interpreter.invoke()
+            
             inference_time = time.perf_counter() - start
-            objs = detect.get_output(interpreter, threshold, scale)
+            #passing the scale from above, this function creates the bounding boxes
+            #it takes the 300x300 image and divides the scale ratio for original coordinates
+            objs = detect.get_objects(interpreter, threshold, scale)
             output = []
             for obj in objs:
                 label = labels.get(obj.id, obj.id)
-                labelID = obj[0]
-                score = obj[1]
-                bbox = obj[2]
+                labelID = obj.id
+                score = obj.score
+                bbox = obj.bbox
                 output.append({"bbox": bbox, "class": label, "score": score})
+            #outputted data is based on original feed in image size
             printData(output, (inference_time * 1000))
         except Exception as e:
             printError(str(e))
