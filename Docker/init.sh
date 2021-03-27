f20a992969efd77619cfaf5fbbbf0b8a703a4b31
Moe <github@m03.ca>
2021-03-27 07:27:04 -0700

    Fix database designation for docker image

--- Docker/init.sh
+++ Docker/init.sh
@@ -62,7 +62,7 @@ if [ "$DB_DISABLE_INCLUDED" = "false" ]; then
     echo "Setting up MySQL database if it does not exists ..."
 
     echo "Create database schema if it does not exists ..."
-    mysql -e "source /home/Shinobi/sql/framework.sql" || true
+    mysql -e "source /home/Shinobi/sql/framework1.sql" || true
 
     echo "Create database user if it does not exists ..."
     mysql -e "source /home/Shinobi/sql/user.sql" || true
@@ -103,7 +103,7 @@ fi
 
 if [ -e "/config/init.extension.sh" ]; then
     echo "Running extension init file ..."
-    ( sh /config/init.extension.sh ) 
+    ( sh /config/init.extension.sh )
 fi
 
 # Execute Command
