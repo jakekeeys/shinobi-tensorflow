bf1bb50d0c57780039908e8ad1b9c8b21aca1606
Moe <github@m03.ca>
2021-04-04 10:37:17 -0700

    Update dash2.googleSignIn.js

--- web/libs/js/dash2.googleSignIn.js
+++ web/libs/js/dash2.googleSignIn.js
@@ -1,8 +1,12 @@
 $(document).ready(function(){
     $('#settings').on('click','.google-sign-in',function(){
         var signInWindow = window.open(getApiPrefix('loginTokenAddGoogle'),'popup','width=300,height=300,scrollbars=no,resizable=no');
-        signInWindow.onbeforeunload = function(){
-            drawAlternateLoginsToSettings()
+        if(!signInWindow || signInWindow.closed || typeof signInWindow.closed=='undefined'){
+            alert(`Your Popup Blocker is disabling this feature.`)
+        }else{
+            signInWindow.onbeforeunload = function(){
+                drawAlternateLoginsToSettings()
+            }
         }
         return false;
     })
