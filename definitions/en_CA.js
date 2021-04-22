ba9bdb665d3e5c1de94b09e839cc2aa3609f33fa
Moe <github@m03.ca>
2021-04-22 09:45:20 -0700

    Add Schedules to definitions

--- definitions/en_CA.js
+++ definitions/en_CA.js
@@ -5512,7 +5512,6 @@ module.exports = function(s,config,lang){
                "name": lang["Points"],
                "color": "orange",
                "section-pre-class": "col-md-6",
-               "sectionClass": "where",
                "blockquoteClass": "global_tip",
                "blockquote": lang.RegionNote,
                "info": [
@@ -5525,6 +5524,137 @@ module.exports = function(s,config,lang){
                ]
             }
          }
-     }
+     },
+     "Schedules": {
+         "section": "Schedules",
+         "blocks": {
+             "Schedules": {
+                "name": lang["Schedules"],
+                "color": "orange",
+                "section-pre-class": "col-md-6",
+                "info": [
+                    {
+                       "id": "schedulesSelector",
+                       "field": lang["Schedules"],
+                       "fieldType": "select",
+                       "possible": [
+                           {
+                              "name": lang['Add New'],
+                              "value": ""
+                           },
+                           {
+                              "name": lang.Saved,
+                              "optgroup": []
+                           },
+                       ]
+                   },
+                ]
+            },
+            "Schedule": {
+               "name": lang["Schedule"],
+               "headerTitle": `${lang['Schedule']} %>
+                 <div class="pull-right">
+                     <a class="btn btn-danger btn-xs delete" style="display:none">&nbsp;<i class="fa fa-trash-o"></i>&nbsp;</a>
+                 </div>`,
+               "color": "green",
+               "section-pre-class": "col-md-6",
+               "info": [
+                   {
+                      "name": "name",
+                      "field": lang.Name,
+                      "description": "",
+                      "example": "Motion Off",
+                      "possible": ""
+                   },
+                   {
+                      "name": "enabled",
+                      "field": lang.Enabled,
+                      "default": "1",
+                      "fieldType": "select",
+                      "possible": [
+                          {
+                             "name": lang.No,
+                             "value": "0"
+                          },
+                          {
+                             "name": lang.Yes,
+                             "value": "1"
+                          }
+                      ]
+                   },
+                   {
+                      "name": "timezone",
+                      "field": lang['Timezone Offset'],
+                      "default": config.timeZones.find(tz => !!tz.selected).value,
+                      "fieldType": "select",
+                      "possible": config.timeZones.map((tz) => {
+                          return {
+                              "name": tz.text,
+                              "value": tz.value
+                          }
+                      })
+                   },
+                   {
+                      "name": "start",
+                      "field": lang.Start,
+                      "description": "",
+                      "placeholder": "HH:mm",
+                      "possible": "1:00"
+                   },
+                   {
+                      "name": "end",
+                      "field": lang.End,
+                      "description": "",
+                      "placeholder": "HH:mm",
+                      "possible": "2:00"
+                   },
+                   {
+                      "name": "days",
+                      "field": lang.Days,
+                      "default": "0",
+                      "fieldType": "select",
+                      "attribute": "multiple",
+                      "possible": [
+                          {
+                             "name": lang.Sunday,
+                             "value": "0"
+                          },
+                          {
+                             "name": lang.Monday,
+                             "value": "1"
+                          },
+                          {
+                             "name": lang.Tuesday,
+                             "value": "2"
+                          },
+                          {
+                             "name": lang.Wednesday,
+                             "value": "3"
+                          },
+                          {
+                             "name": lang.Thursday,
+                             "value": "4"
+                          },
+                          {
+                             "name": lang.Friday,
+                             "value": "5"
+                          },
+                          {
+                             "name": lang.Saturday,
+                             "value": "6"
+                          },
+                      ]
+                   },
+                   {
+                      "name": "monitorStates",
+                      "field": lang['Monitor States'],
+                      "fieldType": "select",
+                      "attribute": `multiple style="min-height:100px"`,
+                      "possible": []
+                   },
+               ]
+           },
+         }
+     },
     }
 }
